61a9403c7263781d0714bdf8e4aa61c493ee9ce9
André Mendes <andregmendes@gmail.com>
2023-08-17 16:53:11 +0100

    (DOM - Changes made in lesson  6, chapter 12 : Replacing Elements)

--- 06-document-object-model/12-replacing-elements/shopping-list/script.js
+++ 06-document-object-model/12-replacing-elements/shopping-list/script.js
@@ -20,7 +20,8 @@ function replaceAllItems() {
   const lis = document.querySelectorAll('li');
 
   // lis.forEach((item, index) => {
-  //   // item.outerHTML = '<li>Replace All</li>';
+    // item.outerHTML = '<li>Replace All</li>';
+    
   //   if (index === 1) {
   //     item.innerHTML = 'Second Item';
   //   } else {
@@ -40,8 +41,8 @@ function replaceChildHeading() {
   const h1 = document.querySelector('header h1');
 
   const h2 = document.createElement('h2');
-  h2.id = 'app-title';
-  h2.textContent = 'Shopping List';
+  // h2.id = 'app-title';
+  h2.textContent = 'Shopping List Special';
   header.replaceChild(h2, h1);
 }
 
