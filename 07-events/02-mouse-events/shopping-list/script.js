ef7aba94d9e054cc2bb695321b0d700f6b4cca63
André Mendes <andregmendes@gmail.com>
2023-08-17 19:52:49 +0100

    (EVENTS - Lesson 7 - Chapter 2 : Mouse Events)

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,15 +1,17 @@
 const logo = document.querySelector('img');
 
 const onClick = () => console.log('click event');
+
 const onDoubleClick = () => {
   if (document.body.style.backgroundColor !== 'purple') {
     document.body.style.backgroundColor = 'purple';
     document.body.style.color = 'white';
-  } else {
+  } else
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
-  }
+ 
 };
+
 const onRightClick = () => console.log('right click event');
 const onMouseDown = () => console.log('mouse down event');
 const onMouseUp = () => console.log('mouse up event');
@@ -32,3 +34,64 @@ logo.addEventListener('mouseout', onMouseOut);
 logo.addEventListener('dragstart', onDragStart);
 logo.addEventListener('drag', onDrag);
 logo.addEventListener('dragend', onDragEnd);
+
+
+
+
+// ------------------ CUSTOM Andy
+
+const clearBtn = document.querySelector('#clear');
+
+clearBtn.addEventListener('click', onClear);
+
+function onClear() {
+  
+  document.
+    querySelector('#item-list-animals').
+      querySelectorAll('li').forEach((item) => item.remove());
+  
+}
+
+
+
+
+const onDoubleClick2 = () => {
+  if (document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  } else
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+ 
+};
+
+
+clearBtn.addEventListener('dblclick', onDoubleClick2);
+
+
+
+
+// let hasColor = document.body.style.backgroundColor !== 'purple'; 
+// let backgroundColor = document.body.style.backgroundColor = 'white';
+// let textColor = document.body.style.color = 'black'
+
+
+// const onDoubleClick3 = () => {
+
+//   switch (backgroundColor) {
+//     case 'purple' : 
+//       document.body.style.backgroundColor = 'white';
+//       document.body.style.color = 'black';
+//       break;
+    
+//     case 'white' : 
+//       document.body.style.backgroundColor = 'purple';
+//       document.body.style.color = 'white';
+//       break;
+    
+//     default: console.log('No other color')
+//   }
+
+// };
+
+// clearBtn.addEventListener('dblclick', onDoubleClick3);
