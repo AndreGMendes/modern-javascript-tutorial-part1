d33e697b5cd1a437d65c88b0497bccb21e2b0d0a
André Mendes <andregmendes@gmail.com>
2023-08-17 01:32:59 +0100

    (DOM - Changes made in lesson  6, chapter 2)

--- 06-document-object-model/01-dom-intro/script.js
+++ 06-document-object-model/01-dom-intro/script.js
@@ -15,6 +15,7 @@ console.log(document.links[0]);
 document.write('Hello from JS');
 
 // We also have methods to select elements more directly
-document.getElementById('main').innerHTML = '<h1>Hello from main</h1>';
+document.getElementById('main').innerHTML = '<h1>Hello from main</h1>' 
+
 
 document.querySelector('#main h1').innerText = 'Hello';
--- 06-document-object-model/02-examining-document-object-properties/shopping-list/script.js
+++ 06-document-object-model/02-examining-document-object-properties/shopping-list/script.js
@@ -30,24 +30,24 @@ output = document.forms[0].id;
 output = document.forms[0].method;
 output = document.forms[0].action;
 
-// Change a form id
+// -- Change a form id
 document.forms[0].id = 'new-id';
 
 // Get all links
-// output = document.links;
-// output = document.links[0];
-// output = document.links[0].href;
-// output = document.links[0].href = 'https://facebook.com';
-// output = document.links[0].id = 'google-link';
-// output = document.links[0].className = 'google-class';
-// output = document.links[0].classList;
+output = document.links;
+output = document.links[0];
+output = document.links[0].href;
+output = document.links[0].href = 'https://facebook.com';
+output = document.links[0].id = 'google-link';
+output = document.links[0].className = 'google-class';
+output = document.links[0].classList;
 
-// Get all images
+// -- Get all images
 output = document.images;
 output = document.images[0];
 output = document.images[0].src;
 
-// Turn an HTMLCollection into an array
+// -- Turn an HTMLCollection into an array
 const forms = Array.from(document.forms);
 forms.forEach((form) => console.log(form));
 
