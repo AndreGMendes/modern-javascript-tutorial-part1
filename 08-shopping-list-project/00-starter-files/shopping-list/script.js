900d8c2b8d1bbeef5f98e4ebaaa48cc0be510373
André Mendes <andregmendes@gmail.com>
2023-08-18 01:06:38 +0100

    Section 08: Chapter 01 - SHOPPING LIST Project: Adding/removing/clearing elements in the Shopping List

---
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -0,0 +1,71 @@
+// DOM VARIABLES
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
+
+// MAIN FUNCTIONS
+function addItem (e) {
+    
+    // to prevent 'Event Bubbling'
+    e.preventDefault();
+
+    // Validation
+    newItem = itemInput.value;
+    if (newItem === '') {
+        alert('Please add an item')
+        return;
+    }
+
+    // Create Parent element
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create Child elements
+    const button = createButton ('remove-item btn-link text-red');
+    
+    // Append the button to the List Element
+    li.appendChild(button);
+    
+    // Append the list element to the List of elements 
+    itemList.appendChild(li); 
+
+    // Clear the input
+    itemInput.value = '';
+}
+
+function removeItem (e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearAllItems (e) {
+    itemList.remove();
+    itemInput.value = null;
+}
+
+// ELEMENT FUNCTIONS
+function createButton (classes) {
+    const button = document.createElement('button')
+    button.className = classes;
+
+    // Apend icon to button
+    const icon = createIcon ('fa-solid fa-xmark');
+    button.appendChild(icon);
+
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i')
+    icon.className = classes;
+    return icon;
+}
+
+
+
+// EVENT LISTENERS
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearAllItems);
