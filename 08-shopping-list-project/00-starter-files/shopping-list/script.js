a925c66a800ca97aa9e1043e326ba604b5328eb3
André Mendes <andregmendes@gmail.com>
2023-08-18 01:35:04 +0100

    Section 08: Chapter 03 - SHOPPING LIST Project: checking for changes in the UI after removing/inserting or clearing all elements

--- 08-shopping-list-project/00-starter-files/shopping-list/script.js
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -1,10 +1,11 @@
-// DOM VARIABLES
+// DOM VARIABLES ---------------------------------------------
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter')
 
-// MAIN FUNCTIONS
+// MAIN FUNCTIONS --------------------------------------------
 function addItem (e) {
     
     // to prevent 'Event Bubbling'
@@ -30,22 +31,49 @@ function addItem (e) {
     // Append the list element to the List of elements 
     itemList.appendChild(li); 
 
+
+    // Check the UI for adding back the Filter and the Clear button
+    checkUIState();
+
     // Clear the input
     itemInput.value = '';
 }
 
 function removeItem (e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
     }
+    checkUIState();
 }
 
 function clearAllItems (e) {
-    itemList.remove();
-    itemInput.value = null;
+    // itemList.remove();
+    // itemInput.value = null;
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+    checkUIState();
 }
 
-// ELEMENT FUNCTIONS
+
+// STATE FUNCTIONS -------------------------------------------
+function checkUIState () {
+
+    const items = document.querySelectorAll('li');
+
+    if (items.length === 0) {
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
+
+// ELEMENT FUNCTIONS -----------------------------------------
 function createButton (classes) {
     const button = document.createElement('button')
     button.className = classes;
@@ -64,8 +92,11 @@ function createIcon(classes) {
 }
 
 
-
-// EVENT LISTENERS
+// EVENT LISTENERS -------------------------------------------
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearAllItems);
+
+
+// AUTOMATIC FUNCTIONS ---------------------------------------
+checkUIState();
