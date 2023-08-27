04ae0bc2fa1d0b907d7b94c8689a4df8f21483d0
André Mendes <andregmendes@gmail.com>
2023-08-27 03:10:37 +0100

    Request Animation Frame intro

--- 12-web-browser-apis/04-requestAnimationFrame/script.js
+++ 12-web-browser-apis/04-requestAnimationFrame/script.js
@@ -1,51 +1,65 @@
 // ------------------------------------------------------------
 // RECURSIVE FUNCTION -----------------------------------------
 // ------------------------------------------------------------
+// const myfunction = () => {
+//   const maximumRecursiveFunctionCalls = 10000
+//   recursiveFunctionCallNumber = 0
 
+//   while (recursiveFunctionCallNumber < maximumRecursiveFunctionCalls) {
+//     recursiveFunctionCallNumber++;
+//     console.log(`Cycle nº ${recursiveFunctionCallNumber}`)
+//   }
+//   recursiveFunctionCallNumber !== maximumRecursiveFunctionCalls ? myfunction() : console.log(`Recursive Function Call ended after ${recursiveFunctionCallNumber} calls`);
+// }
 
+// myfunction();
 
-const myfunction = () => {
-  const maximumRecursiveFunctionCalls = 10000
-  recursiveFunctionCallNumber = 0
+// ------------------------------------------------------------
+// RETURN EXAMPLE ---------------------------------------------
+// ------------------------------------------------------------
+// function ze (name) {
+//   for (z = 0; z <= 10; z++) {
+//     if (name === 'André') {
+//       console.log(name, z)
+//     } else {
+//       console.log('function has returned', z)
+//       return;
+//     }
+//   }
+// }
 
-  while (recursiveFunctionCallNumber < maximumRecursiveFunctionCalls) {
-    recursiveFunctionCallNumber++;
-    console.log(`Cycle nº ${recursiveFunctionCallNumber}`)
-  }
- 
-  recursiveFunctionCallNumber !== maximumRecursiveFunctionCalls ? myfunction() : console.log(`Recursive Function Call ended after ${recursiveFunctionCallNumber} calls`);
-}
-
-myfunction();
+// ze('AndréX');
 
 
 
 
 // ------------------------------------------------------------
-// const image = document.querySelector('img');
-// let start;
-// let done = false;
+const image = document.querySelector('img');
+let start;
+let done = false;
 
-// function step(timestamp) {
-//   if (start === undefined) {
-//     start = timestamp;
-//   }
+function step(timestamp) {
 
-//   const elapsed = timestamp - start;
+  // console.log(timestamp);
 
-//   if (elapsed > 5000) {
-//     done = true;
-//   }
+  if (start === undefined) {
+    start = timestamp;
+  }
 
-//   if (done) {
-//     return;
-//   }
+  const elapsed = timestamp - start;
 
-//   image.style.transform = `translateX(${elapsed / 10}px) rotate(${
-//     elapsed / 10
-//   }deg)`;
+  if (elapsed > 5000) {
+    done = true;
+    return
+  }
 
-//   requestAnimationFrame(step);
-// }
+  // if (done) {
+  //   return;
+  // }
 
-// requestAnimationFrame(step);
+  image.style.transform = `translateX(${elapsed / 6}px) rotate(${elapsed / 2}deg)`;
+
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
--- XX-ANDY/script_Recursive_Functions.js
+++ XX-ANDY/script_Recursive_Functions.js
@@ -1,7 +1,6 @@
 // ------------------------------------------------------------
 // RECURSIVE FUNCTION -----------------------------------------
 // ------------------------------------------------------------
-
 const myfunction = () => {
     const maximumRecursiveFunctionCalls = 10000
     recursiveFunctionCallNumber = 0
@@ -13,6 +12,25 @@ const myfunction = () => {
    
     recursiveFunctionCallNumber !== maximumRecursiveFunctionCalls ? myfunction() : console.log(`Recursive Function Call ended after ${recursiveFunctionCallNumber} calls`);
   }
-  
+
   myfunction();
+
+
+// ------------------------------------------------------------
+// RETURN EXAMPLE ---------------------------------------------
+// ------------------------------------------------------------
+  function ze (name) {
+  
+    for (z = 0; z <= 10; z++) {
+  
+      if (name === 'André') {
+        console.log(name, z)
+      } else {
+        console.log('function has returned', z)
+        return;
+      }
+    }
+  }
+  
+  ze('AndréX');
   
