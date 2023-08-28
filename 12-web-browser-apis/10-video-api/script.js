11eb89942089b215df768f6ea00b04d43da6e182
André Mendes <andregmendes@gmail.com>
2023-08-28 18:38:08 +0100

    VIDEO BACKGROUND project added

--- 12-web-browser-apis/10-video-api/script.js
+++ 12-web-browser-apis/10-video-api/script.js
@@ -4,14 +4,23 @@ const pause = document.getElementById('pause');
 const stop = document.getElementById('stop');
 const currentTime = document.getElementById('current-time');
 
+
 play.addEventListener('click', () => video.play());
+
 pause.addEventListener('click', () => video.pause());
+
 stop.addEventListener('click', () => {
   video.pause();
   video.currentTime = 0;
 });
 
-video.addEventListener(
-  'timeupdate',
-  () => (currentTime.innerText = video.currentTime)
-);
+video.addEventListener('timeupdate', () => (currentTime.innerText = video.currentTime));
+
+
+
+// --------------------
+(function updateX () {
+  setInterval (() => {
+  console.log(`Video Current time: ${parseInt(video.currentTime)}s`)
+},1000)
+})()
